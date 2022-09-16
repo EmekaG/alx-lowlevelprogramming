8fea3bc6f066350f412b04e46e761f9eff4f7584
EmekaG <nellychukwemeka@gmail.com>
2022-09-16 14:35:07 -0700

    main.h

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+/**
+ * File - main.h
+ *
+ * Desciption - This is a header file containing all the prototypes of my 0x04-more_functions_nested_loops project.
+ */
+
+int _putchar(char c);
+int _supper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
+#endif
